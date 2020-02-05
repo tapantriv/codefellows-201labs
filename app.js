a2964984b1c4beb365c94031060967ff28173289
robbymo81 <robert.l.mosher@gmail.com>
2020-02-04 20:57:46 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+//var guestName = guestName.toUpperCase();
+var guestName = prompt ('Hi there friend. What is your Name!');
+  alert('Nice to meet you ' + guestName);
+    console.log('guest name is ' + guestName);
+
+
+//var blind = prompt('Great to meet you ' + guestName + ' are you blind?'); 
+//if(blind.toLowerCase() === 'yes'||blind.toLowerCase() ==='y');{
+  // alert('Sorry you cannot continue ' + guestName);
+  //}
+  //else if(blind.toLowerCase() === 'no'||blind.toLowerCase() ==='n');{
+ //   alert('You can ready the next statement');
+//  }
+ // else if (blind === '');
+  //{
+    //alert('Sorry not a valid answer');
+  //}
+  //else
+  //{
+  //  alert('Great next questions');
+  //}
+  
+
+  //var hungry = prompt('Next question ' + guestName + ' Are you Hungry?');
+   // if(hungry.toLowerCase() === 'yes'||hungry.toLowerCase() === 'y');{
+      //alert('Sorry to hear that ' + guestName + ' Continue');
+    //}
+    //else if(hungry.toLowerCase() === 'no'||hungry.toLowerCase() === 'n');
+   //{
+     //alert('Awesome ' + guestName + ' moving on');
+   //}
+    //else if (hungry === '');
+  //{
+   // alert('Sorry not a valid answer');
+  //}
+ // else
+  //{
+   // alert('Question third');
+  //}
+
+  //var happy = prompt('Third question. ' + guestName + ' Are you Happy today?');
+   // if(happy.toLowerCase() === 'yes'||happy.toLowerCase() === 'y');
+  //{
+    //alert('That is Awesome ' + guestName + ' right on');
+  //}
+  //else if(happy.toLowerCase() === 'no'||happy.toLowerCase() === 'n');
+  //{
+    //alert('Think of Peguins ' + guestName + ' lets continue');
+  //}
+   // else if (happy.toLowerCase() === '');
+    //{
+      //alert('Sorry ' + guestName + ' not a valid answer');
+    //}
+    //else
+    //{
+      //alert('eve of the last question');
+    //}
+
+  var sleep = prompt('Almost there ' + guestName + ' Are you Sleep?');
+
+    if(sleep.toLowerCase() === 'yes'||sleep.toLowerCase() ==='y');{
+      alert('Lets take a Nap ' + guestName + ' WAKE UP...nap is over');
+    }
+    else if(sleep.toLowerCase() === 'no'||sleep.toLowerCase() === 'n');{
+      alert('Good job getting your zzzz ' + guestName + ' almost there');
+    }
+    else if(sleep === '');{
+      alert('Sorry ' + guestName + ' not a valid answer');
+    }
+    //else();
+      }
+      //alert('Glad you made it this far' + guestName +);
+    //}      
+    }
+  //prompt('Last question ' + guestName + ' Are you ready to know ME!?');
+//Tapankumar Trivedi helped me on the fist block of if else if else statements
+
